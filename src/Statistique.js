34c5a9dbfe9b8e1ced8e695e635bf6a6df39f900
Abdoulaye Gueye <gueyeabdou2310@gmail.com>
2026-05-02 03:07:00 +0100

    Lab 1 - Exercices : Ajout des composants Footer et Statistique, modification du Header avec la date

---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+  return (
+    <div className="statistique">
+      <div className="statistique-chiffre">10</div>
+      <div className="statistique-libelle">Lignes</div>
+    </div>
+  );
+}
+
+export default Statistique;
